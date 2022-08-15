b2408f00b30e8bd29206ff55504c45b6289e350b
Serhii Haponov <serhii.haponov@chisw.com>
2022-08-15 07:56:27 +0300

    start 4 task

--- Algorithms_HW2/ViewController.swift
+++ Algorithms_HW2/ViewController.swift
@@ -301,7 +301,23 @@ fileprivate extension ViewController {
 // MARK: - Task 4
 //4) В ігровій індустрії часто доводиться мати справу з тригонометричними ф-ми.
 //Ваша задача пришвидшити код який обчислює sin(x) за допомогою ряду Тейлора.
-//Типові приклади оптимізацій тут.
+//Типові приклади оптимізацій https://ocw.mit.edu/courses/6-172-performance-engineering-of-software-systems-fall-2018/resources/lecture-2-bentley-rules-for-optimizing-work/.
+
+//void sinx(int N, int terms, float[] x, float[] result) {
+//      for (int i = 0; i < N; i++) {
+//          float value = x[i];
+//          float numer = x[i] * x[i] * x[i];
+//          int denom = 6; // 3!
+//          int sign = -1;
+//          for (int j = 1; j <= terms; j++) {
+//               value += sign * numer / denom;
+//               numer *= x[i] * x[i];
+//               denom *= (2*j+2) * (2*j+3);
+//               sign *= -1;
+//          }
+//          result[i] = value;
+//     }
+//}
 fileprivate extension ViewController {
     
 }
